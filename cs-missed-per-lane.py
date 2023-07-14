d5bbfa613dda827446bad0cfd12339c8e49b39f7
Charms <carmenferrone5@gmail.com>
2023-07-14 14:55:41 -0400

    updated user interface and statistics row to now show % total CS missed for each column

--- cs-missed-per-lane.py
+++ cs-missed-per-lane.py
@@ -1,29 +1,60 @@
-import pandas as pd
+import csv
+import os
 
-csMissedPerLane = {
-    "failedLastHit": 0,
-    "zonedOrTraded": 0,
-    "resetOrRoamed": 0,
-    "died": 0
-}
 
-while True:
-    user_input = input("Enter a number between 1-4, or 'quit' to exit: ")
+def main():
+    # Define reasons dictionary
+    reasons = {
+        1: "Failed Last Hit",
+        2: "Traded or Zoned",
+        3: "Reset or Roamed",
+        4: "Died",
+        5: "Total CS",
+    }
+    counts = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+    print("Enter the corresponding number each time you miss a CS:")
+    print(
+        "1 - Failed Last Hit\n2 - Traded or Zoned\n3 - Reset or Roamed\n4 - Died\ne - finish logging"
+    )
 
-    if user_input == "quit":
-        total = sum(csMissedPerLane.values())
-        csMissedPerLane["total"] = total
-        percentages = {k: "{}%".format(v/total*100)
-                       for k, v in csMissedPerLane.items()}
-        df = pd.DataFrame([csMissedPerLane, percentages])
-        df.to_excel("cs_missed_per_lane.xlsx", index=False)
-        print("Counters saved to cs_missed_per_lane.xlsx")
-        break
-    elif user_input in ["1", "2", "3", "4"]:
-        csMissedPerLane["failedLastHit"] += int(user_input == "1")
-        csMissedPerLane["zonedOrTraded"] += int(user_input == "2")
-        csMissedPerLane["resetOrRoamed"] += int(user_input == "3")
-        csMissedPerLane["died"] += int(user_input == "4")
-        print(csMissedPerLane)
-    else:
-        print("Invalid input, please enter a number between 1-4 or 'quit'")
+    while True:
+        user_input = input("Input: ")
+
+        # Check if input is "e" for end of logging
+        if user_input.lower() == "e":
+            counts[5] = getFinalCS(False)
+            break
+        # Validate user input
+        elif not user_input.isdigit() or int(user_input) not in [1, 2, 3, 4]:
+            print("Invalid input. Please enter a number from 1-4, or 'e' to finish.")
+        else:
+            counts[int(user_input)] += 1
+
+    # Check if file exists, if not, write headers
+    if not os.path.isfile("Missed_CS_Data.csv"):
+        with open("Missed_CS_Data.csv", mode="w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(reasons.values())  # Write the headers
+
+    # Append counts to csv
+    with open("Missed_CS_Data.csv", mode="a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([counts[key] for key in sorted(counts.keys())])
+        writer.writerow(
+            [
+                f"{round((100 * counts[key]) / (sum(counts.values())), 1)}%"
+                for key in sorted(counts.keys())
+            ]
+        )
+
+
+def getFinalCS(invalidInput):
+    cs = input(
+        "invalid Input, please enter and Integer:\nTotal CS:"
+        if invalidInput
+        else "Total CS:"
+    )
+    return int(cs) if cs.isdigit() else getFinalCS(True)
+
+
+main()
